98ba48d3b78d454f058d018b670f0ee0cf2341e2
Coees <95752342+Coees@users.noreply.github.com>
2021-12-08 19:35:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Gửi Bé Yêu ",
-    introTitle: 'Mai Chi',
+    titleWeb: "Gửi Bạn Iuu <33 ",
+    introTitle: 'Hằng Ó Hỏ',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao hằng cứ đi nhầm đường
+    Lạc vào tim tớ lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'Ấn Vô :))',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Phải chăng tớ đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng tớ đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu Anh <3',
-    btnReply: 'Gửi cho Anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu Em Bé nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 22h, anh sẽ gọi cho em.',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu tớ <3',
+    btnReply: 'Gửi cho tớ  <3',
+    reply: 'Yêu chứ <3',
+    mess: 'Tớ biết mà 🥰. Yêu Cậu nhiều nhiều 😘😘',
+    messDesc: 'Chút chơi game nhaaa <33 .',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/du.om.5074/' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/du.om.5074/'
 }
